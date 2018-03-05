51655868c8cfcbf9d1b1113314df77bc10899827
Le Thanh Tam <thanhtam.jane@gmail.com>
2018-03-05 10:58:41 +0100

    question 10 Eloquent

--- app/Http/Controllers/HeroController.php
+++ app/Http/Controllers/HeroController.php
@@ -8,7 +8,10 @@ class HeroController extends Controller
 {
     public function index()
     {
-        $view = view('hero.index');
+        //In the index method of the HeroController, use Eloquent to select all heroes in the database, ordered by their names in ascending order.
+        $heros = \App\Hero::all()->orderBy('name', 'asc');
+        //Then give the result to the view.
+        $view = view('hero.index',['heros'=>$heros]);
         return $view;
     }
     public function show($hero_slug)
