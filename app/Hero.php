5852112d1587a1a19da6a5dd53217677bc4b68cf
Le Thanh Tam <thanhtam.jane@gmail.com>
2018-03-05 10:51:21 +0100

    question 9 Models

--- app/Hero.php
+++ app/Hero.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Hero extends Model
 {
-    //
+    public function image()
+    {
+        return $this->belongsToMany('Image','hero_image');
+    }
 }
